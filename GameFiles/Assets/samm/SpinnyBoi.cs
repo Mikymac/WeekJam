8c76ddc078dfccdde4ad5f38a3941d1c7e3276c4
Mikymac <mikymacmc@hotmail.co.uk>
2019-01-18 17:08:56 +0000

    Jam Finished
    Spent the day fixing dash, then decided its probably a good point to finish due to achieving plenty of things and deciding we;ve got plenty out of it.

---
+++ GameFiles/Assets/samm/SpinnyBoi.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpinnyBoi : MonoBehaviour {
+
+    public Vector3 Spinny;
+
+    public int maxAng, minAng;
+    bool dir;
+    Camera cam;
+	// Use this for initialization
+	void Start () {
+        cam = GetComponent<Camera>();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        transform.Rotate(Spinny);
+        if (dir)
+        {
+            cam.fieldOfView += Time.fixedDeltaTime * 2;
+            if (cam.fieldOfView >= maxAng)
+            {
+                dir = !dir;
+            }
+        } else
+        {
+            cam.fieldOfView -= Time.fixedDeltaTime * 2;
+            if (cam.fieldOfView <= minAng)
+            {
+                dir = !dir;
+            }
+        }
+    }
+}
